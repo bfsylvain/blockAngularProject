d85f6ba8d5c798e75d67e5814977293349b9d43c
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-11 09:30:58 +0200

    router exercise done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { FormsModule } from '@angular/forms';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MenuComponent } from './menu/menu.component';
+// imports routerOutlet ?? RouterLink ?? Pas besoin ??
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockComponent,
     UserProfileComponent,
+    SignUpComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
