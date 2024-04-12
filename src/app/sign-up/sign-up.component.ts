a4cfb4aab3af43fc407b31895f5ef49a71540237
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-12 19:55:50 +0200

    signup updates

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
-
+  signUpForm: Form = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    };
+    
+    minLengthWord: number = 5
+    maxLengthWord: number = 15
+    minLengthErrorSentence: string= "";
+    maxLengthErrorSentence: string = "";
+    
+    ngOnInit(){
+      this.generateLengthErrorMessages(this.minLengthWord, this.maxLengthWord)
+    }
+    generateLengthErrorMessages(minLength: number, maxLength: number) {
+      this.minLengthErrorSentence = `doit faire ${minLength} caractères minimum`
+      this.maxLengthErrorSentence = `doit faire ${maxLength} caractères maximum`
+    }
+    onSubmit() {
+      console.log(this.minLengthErrorSentence);
+    }
 }
