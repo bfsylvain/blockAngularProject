49c49a6bbed9f9ded22eabae0ee5e3e87c5fa6eb
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-17 17:21:04 +0200

    updates custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { DeveloperComponent } from './developer/developer.component';
 import { SkillsComponent } from './skills/skills.component';
 import { CreateOnomatopoeiaComponentComponent } from './create-onomatopoeia-component/create-onomatopoeia-component.component';
@@ -19,8 +17,6 @@ import { AddressPipe } from './address.pipe';
   declarations: [
     AppComponent,
     BlockComponent,
-    UserProfileComponent,
-    SignUpComponent,
     DeveloperComponent,
     SkillsComponent,
     CreateOnomatopoeiaComponentComponent,
