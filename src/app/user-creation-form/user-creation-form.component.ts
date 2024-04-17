7276dba3de4ae8c781ed64bbf2497de375cd7bb5
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-17 10:07:57 +0200

    form creation

--- src/app/user-creation-form/user-creation-form.component.ts
+++ src/app/user-creation-form/user-creation-form.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { NewUser } from '../models/types/newUser';
 
 @Component({
   selector: 'app-user-creation-form',
   templateUrl: './user-creation-form.component.html',
-  styleUrl: './user-creation-form.component.scss'
+  styleUrl: './user-creation-form.component.scss',
 })
 export class UserCreationFormComponent {
-form: FormGroup = new FormGroup({
-  name: new FormControl(''),
-  email: new FormControl(''),
-  address: new FormControl(''),
-  
-})
+  user: NewUser = {
+    name:"",
+    email:"",
+    password:"",
+    address:""
+  };
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    addressNumber: new FormControl(''),
+    address: new FormControl(''),
+    zipCode: new FormControl(''),
+  });
+
+  onClick() {
+    const formData = this.form.value;
+  }
 }
